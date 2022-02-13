63ec43c1ad91436e040e96e2cb5625b2bcf00d65
EssaddekOthmane <87573896+EssaddekOthmane@users.noreply.github.com>
2022-02-13 16:29:29 +0100

    Update APP.py

--- APP.py
+++ APP.py
@@ -174,7 +174,7 @@ class classifieur:#(str):#,par,X_trai,Y_train,X_test,Y_test):
     self.train_classifieur(X_train,Y_train)
     erreurs = list(abs(self.algo.predict(X) - y))
     zones_err =[i*(erreurs[k]==1) for k in range(len(y))] #[k for k in range(len(erreurs)) if erreurs[k] == 1]
-    return zones_err, [i for k in range(len(y))]
+    return zones_err, [k for k in range(len(y))]
   
   
   
